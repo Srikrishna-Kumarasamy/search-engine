53fdaee854094484c60fa5cdc017ee7144cce02b
sriram M <52700162+rockramsri@users.noreply.github.com>
2024-10-27 23:11:18 -0400

    Query and Compression file changes

--- io_efficient_merge_sort.cpp
+++ io_efficient_merge_sort.cpp
@@ -3,11 +3,130 @@
 #include<fstream>
 using namespace std;
 
+ostream& operator<<(ostream& os, const vector<unsigned long int>& vec) {
+    os << "[";
+    for (size_t i = 0; i < vec.size(); ++i) {
+        os << vec[i];
+        if (i != vec.size() - 1) {
+            os << ", ";
+        }
+    }
+    os << "]";
+    return os;
+}
+ostream& operator<<(ostream& os, const vector<u_int8_t>& vec) {
+    os << "[";
+    for (size_t i = 0; i < vec.size(); ++i) {
+        os << bitset<8>(vec[i]);
+        if (i != vec.size() - 1) {
+            os << ", ";
+        }
+    }
+    os << "]";
+    return os;
+}
+
+     vector<unsigned long int> mergeDocAndFreqChunks( vector<unsigned long int> docChunkSize, vector<unsigned long int> freqChunkSize  )
+    {
+        vector<unsigned long int> finalChunkList;
+        for(int i=0;i<docChunkSize.size();i++)
+        {
+            finalChunkList.push_back( docChunkSize[i] );
+            finalChunkList.push_back( freqChunkSize[i] );
+        }
+        return finalChunkList;
+    }
+
+    vector<uint8_t> varbyte_encode(unsigned long int number) 
+        {
+        vector<uint8_t> encoded;
+
+        // Continue until the number is less than 128 (fits in 7 bits)
+        while (number >= 128) {
+            // Add 7 bits of the number to the encoded result (MSB = 0)
+            encoded.push_back(static_cast<uint8_t>(number & 127));  // Store the 7 bits
+            number >>= 7;  // Shift right by 7 bits to process the next 7 bits
+        }
+
+        // Add the last 7 bits with MSB = 1 to indicate the end
+        encoded.push_back(static_cast<uint8_t>(number | 128));  // Set MSB to 1
+
+        return encoded;
+        }
+    vector< vector<uint8_t> > get_Varbyte_Encoded_List(vector<unsigned long int> docList,vector<unsigned long int> &chunkSize,unsigned long int &totalSize)
+    {
+
+        vector< vector<uint8_t> > finalCompressedList;
+        vector<uint8_t> finalList;
+        unsigned long int tempSize=0UL;
+        for(unsigned long int i=0;i<docList.size();i++)
+        {
+            vector< uint8_t> compressed_Number_Vector= varbyte_encode(docList[i]);
+            cout<<docList[i]<< " Var byte encoded size :"<< compressed_Number_Vector.size()<<endl;
+            tempSize+=compressed_Number_Vector.size();
+            finalList.insert(finalList.end(), compressed_Number_Vector.begin(),compressed_Number_Vector.end());
+            if( (i+1) % 128 ==0 )
+            {
+                chunkSize.push_back(tempSize);
+                finalCompressedList.push_back(finalList);
+                cout<<"*"<<finalList.size()<<"*";
+                totalSize+=finalList.size();
+                finalList.clear();
+                tempSize=0UL;
+            }
+            
+        }
+        if( docList.size()%128 !=0 )
+        {
+            chunkSize.push_back(tempSize);
+            totalSize+=finalList.size();
+            finalCompressedList.push_back(finalList);
+        }
+        cout<<"="<<totalSize<<"=";
+        return finalCompressedList;
+    }
+
+    
+
+    vector<unsigned long int>  update_and_remove_gaps(vector<unsigned long int> docList,vector<unsigned long int> &lastDocId)
+        {
+            vector<unsigned long int> tempDocList;
+            unsigned long int diff=0UL;
+            for(unsigned long int i=0;i<docList.size();i++)
+            {
+               
+                if ( (i+1)%128 !=1 )
+                {
+                    tempDocList.push_back( docList[i]-docList[i-1] );
+                    if( (i+1)%128==0 )
+                    lastDocId.push_back(docList[i]);
+                }
+                else{
+                    tempDocList.push_back(docList[i]);
+                }
+            }
+            if(docList.size() % 128 !=0 )
+            lastDocId.push_back(docList[docList.size()-1]);
+            return tempDocList;
+            
+        }
+    void store_mini_blocks_In_bin(ofstream& binaryFile,vector< vector<uint8_t> > doc_data,vector< vector<uint8_t> > freq_data)
+    {
+        for(int i=0;i<doc_data.size();i++)
+        {
+          binaryFile.write(reinterpret_cast<const char*>(doc_data[i].data()), doc_data[i].size());
+         binaryFile.write(reinterpret_cast<const char*>(freq_data[i].data()), freq_data[i].size());
+        }
+    }
+
 class Term {
     public:
     string term;
     string doc_frequency;
     int intermediate_file_index;
+
+
+    
 
     Term(string term, string doc_frequency, int intermediate_file_index)
     {
@@ -44,12 +163,79 @@ long get_documents_count_per_term(string document_id_string)
     return doc_count;
 }
 
+                unsigned long int compress(string line,ofstream &bin_file)
+                {
+
+                       
+                        // Example : geographic-specific :1415722 1 1415723 1 1415724 1
+
+                        // Store the Term in lexicon
+                        
+                        // Intialize the MetaData block
+                        vector<unsigned long int> lastDocIds;
+                        vector<unsigned long int> chunksize;
+
+                        // Parse the DocIDs and Freqs
+                        stringstream lineStream(line);
+                        string token,data;
+                        getline(lineStream,token,':');
+                        getline(lineStream,token,':');
+                        stringstream dataStream(token);
+                        vector<unsigned long int> docList;
+                        vector<unsigned long int> freqList;
+                        //cout<< token <<" "<<data;
+                        while(getline(dataStream,data,' '))
+                        {
+                            unsigned long int data_in_num;
+                            istringstream(data) >> data_in_num;
+                            docList.push_back(data_in_num);
+                            getline(dataStream,data,' ');
+                            istringstream(data) >> data_in_num;
+                            freqList.push_back(data_in_num-1);
+
+                        }
+
+                        docList= update_and_remove_gaps(docList,lastDocIds);
+
+                        // Varbyte compression is done for both DocIds and freq
+                        vector<unsigned long int> docIdsChunkSize;
+                        vector<unsigned long int> freqsChunkSize;
+                        unsigned long int compressed_docid_size=0UL;
+                        unsigned long int compressed_freq_size=0UL;
+                        vector< vector<uint8_t> > compressed_doc_id_block_list= get_Varbyte_Encoded_List(docList,docIdsChunkSize,compressed_docid_size);
+                        vector< vector<uint8_t> > compressed_freq_block_list= get_Varbyte_Encoded_List(freqList,freqsChunkSize,compressed_freq_size);
+
+                        unsigned long int lastDocIdList_size= lastDocIds.size() ;
+
+                        vector<unsigned long int> finalChunkSizeList = mergeDocAndFreqChunks(docIdsChunkSize,freqsChunkSize);
+
+                        unsigned long int chunkList_size= finalChunkSizeList.size() ;
+
+                                                        // sizeof last doc  +  last docids size      +  size of 
+                        unsigned long int totalBlockSize = 8                + lastDocIdList_size * 8 + 8 + chunkList_size * 8 + compressed_docid_size + compressed_freq_size;
+                        //
+                         bin_file.write(reinterpret_cast<const char*>(&totalBlockSize), sizeof(totalBlockSize));
+                         bin_file.write(reinterpret_cast<const char*>(&lastDocIdList_size), sizeof(lastDocIdList_size));
+                         bin_file.write(reinterpret_cast<const char*>(lastDocIds.data()), sizeof(unsigned long int) * lastDocIds.size());
+                         bin_file.write(reinterpret_cast<const char*>(&chunkList_size), sizeof(chunkList_size));
+                        bin_file.write(reinterpret_cast<const char*>(finalChunkSizeList.data()), sizeof(unsigned long int) * finalChunkSizeList.size());
+
+                      store_mini_blocks_In_bin(bin_file,compressed_doc_id_block_list,compressed_freq_block_list);
+
+                        cout << lastDocIds<<endl<<docList<<endl<<freqList;
+                        cout<< lastDocIdList_size  <<" "<< chunkList_size   <<" "<< compressed_docid_size <<" "<< compressed_freq_size <<" Total size = "<<totalBlockSize<<endl;
+                        
+                    return totalBlockSize;
+                }
+
 int main() 
 {
     float BATCH_SIZE = 10000;
+    unsigned long int position=0UL;
     int num_of_intermediate_files = ceil(8841823/BATCH_SIZE);
     ofstream inverted_index = ofstream("final_inverted_index.txt");
     ofstream lexicon = ofstream("lexicon.txt");
+    ofstream bin_file("data.bin", ios::binary);
     vector<ifstream> intermediate_files(num_of_intermediate_files);
     priority_queue<Term, vector<Term>> min_heap;
 
@@ -76,8 +262,12 @@ int main()
         if (min_term.term != prev)
         {
             long document_count_per_term = get_documents_count_per_term(doc_freq_combined);
-            inverted_index<<prev<<":"<<doc_freq_combined<<endl;
-            lexicon<<prev<<" "<<term_id<<" "<<document_count_per_term<<endl;
+
+            //inverted_index<<prev<<":"<<doc_freq_combined<<endl;
+
+            position+=compress(prev+":"+doc_freq_combined,bin_file);
+
+            lexicon<<prev<<" "<<term_id<<" "<<document_count_per_term<<" "<<position<<endl;
             doc_freq_combined = min_term.doc_frequency;
             term_id++;
         }
@@ -97,8 +287,10 @@ int main()
         if (terms_processed % 10000 == 0) cout<<"Terms Processed : "<<terms_processed<<endl;
     }
     long document_count_per_term = get_documents_count_per_term(doc_freq_combined);
-    inverted_index<<prev<<":"<<doc_freq_combined<<endl;
-    lexicon<<prev<<" "<<term_id<<" "<<document_count_per_term<<endl;
+    // inverted_index<<prev<<":"<<doc_freq_combined<<endl;
+    position+=compress(prev+":"+doc_freq_combined,bin_file);
+    lexicon<<prev<<" "<<term_id<<" "<<document_count_per_term<<" "<<position;
+    // lexicon<<prev<<" "<<term_id<<" "<<document_count_per_term<<endl;
     lexicon.close();
     inverted_index.close();
     return 0;
--- query.cpp
+++ query.cpp
@@ -28,21 +28,43 @@ ostream& operator<<(ostream& os, const vector<u_int8_t>& vec) {
     return os;
 }
 
+    vector<unsigned long> varbyte_decompress(const vector<uint8_t>& encoded_data) {
+            vector<unsigned long> decompressed_values;
+            unsigned long current_value = 0;
+            int shift = 0;
+
+            for (uint8_t byte : encoded_data) {
+                // Extract the lower 7 bits of the byte and shift into the current value
+                current_value |= (static_cast<unsigned long>(byte & 0x7F) << shift);
+
+                // Check if MSB is 1, indicating the end of this integer
+                if (byte & 0x80) {
+                    decompressed_values.push_back(current_value); // Add the decompressed integer to the result
+                    current_value = 0;  // Reset for the next integer
+                    shift = 0;          // Reset shift for the next integer
+                } else {
+                    shift += 7;         // Increment shift by 7 to handle the next part of the number
+                }
+            }
+
+            return decompressed_values;
+    }
+
         // Position: words position based on the term id from lexicon
         void get_inverted_index_Metadatas_based_on_position(ifstream& bin_in_file,unsigned long int position,unsigned long int &lastdocsize,vector<unsigned long int> &read_Last_docIds_data,unsigned long int &lengthOfChunksizeList,vector<unsigned long int> &read_chunksize_data)
         {
-            for(unsigned long int i=0;i<position-1;i++)
-                {
+            // for(unsigned long int i=0;i<position-1;i++)
+            //     {
                     
-                    unsigned long int totalsize=0UL;
-                    bin_in_file.read(reinterpret_cast<char*>(&totalsize), sizeof(totalsize));
-                    cout<<totalsize<<endl;
-                    cout<<bin_in_file.tellg()<<endl;
+            //         unsigned long int totalsize=0UL;
+            //         bin_in_file.read(reinterpret_cast<char*>(&totalsize), sizeof(totalsize));
+            //         cout<<totalsize<<endl;
+            //         cout<<bin_in_file.tellg()<<endl;
 
-                    //Skipping logic
-                    bin_in_file.seekg( totalsize , ios::cur);
-                    cout<<"Location "<<bin_in_file.tellg()<<endl;
-                }
+            //         //Skipping logic
+            //         bin_in_file.seekg( totalsize , ios::cur);
+            //         cout<<"Location "<<bin_in_file.tellg()<<endl;
+            //     }
 
                 // To retrieve Total size
                 unsigned long int totalsize=0UL;
@@ -70,9 +92,10 @@ ostream& operator<<(ostream& os, const vector<u_int8_t>& vec) {
             }
 
 
-   void get_entire_block_DocIDs_and_Freq(ifstream& bin_in_file,vector<u_int8_t> &entireBlock_DocID ,vector<u_int8_t> &entireBlock_Freq, unsigned long int &lastdocsize, vector<unsigned long int> &read_chunksize_data  )
+   void calculate_score(ifstream& bin_in_file,unsigned long int &lastdocsize, vector<unsigned long int> &read_chunksize_data  ,vector<unsigned long int> queryTermPositions)
    {
        // To retreieve the doclist (Entire)
+       unsigned long int score=0;
         for(unsigned long int i=0; i< lastdocsize ; i++ )
         {
 
@@ -83,57 +106,107 @@ ostream& operator<<(ostream& os, const vector<u_int8_t>& vec) {
             vector<u_int8_t> freq_compressed_list( read_chunksize_data[ (2*i)+1 ] );
             bin_in_file.read(reinterpret_cast<char*>( freq_compressed_list.data()), freq_compressed_list.size());
             cout<<" Freq in compressed : "<<freq_compressed_list;
-        }
-   }
-   
-    void get_mini_block_DocIDs_and_Freq( ifstream& bin_in_file, unsigned long int docID_to_be_searched, vector<u_int8_t> &miniBlock_DocID ,vector<u_int8_t> &miniBlock_Freq,unsigned long int &lastdocsize, vector<unsigned long int> &read_chunksize_data,vector<unsigned long int> &read_Last_docIds_data)
-    {
-         // To retrieve the particular Block based on the DocID
-        // unsigned long int docID_to_be_searched;
-        unsigned long int total_chunk_Size=0UL;
-        for(unsigned long int i=0;i<lastdocsize;i++)
-             {
-        
-             if( docID_to_be_searched < read_Last_docIds_data[i] )
-                 {
 
-                        // Need to swift for reading
-                        bin_in_file.seekg(total_chunk_Size,ios::cur);
+            //Call miniBlock DocID based on DocID search
+            vector<unsigned long int> decompressed_docID_block= varbyte_decompress(docId_compressed_list);
+            vector<unsigned long int> decompressed_freq_block= varbyte_decompress(freq_compressed_list);
+            
+            //Inside block traversal
+            for(int blockIndex=0;blockIndex <  decompressed_docID_block.size();blockIndex++)
+            {
+                //For the indexed word score calcualtion here
 
-                        // Reading that particular block and passing for uncompression (DOCIDs List and Freqs List)
-                        //vector<u_int8_t> miniBlock_DocID( read_chunksize_data[2*i] );
-                        //vector<u_int8_t> miniBlock_Freq( read_chunksize_data[(2*i)+1] );
-                        bin_in_file.read(reinterpret_cast<char*>( miniBlock_DocID.data()), miniBlock_DocID.size());
-                        bin_in_file.read(reinterpret_cast<char*>( miniBlock_Freq.data()), miniBlock_Freq.size());
+                // Score calculation if there are more terms
+                if(queryTermPositions.size()>1)
+                {
+                    for(unsigned long int index=0;index < queryTermPositions.size();index++)
+                    {
+                        ifstream invertedList_term_pointer("data.bin", ios::binary);
+                        unsigned long int lastdocsize=0UL;
+                        vector<unsigned long int> read_Last_docIds_data;
+                        unsigned long int lengthOfChunksizeList=0UL;
+                        vector<unsigned long int> read_chunksize_data;
+                        // To move the pointer for reading in the binary file
+                        bin_in_file.seekg( queryTermPositions[index] , ios::cur);
+                        // For reading Metadata
+                        get_inverted_index_Metadatas_based_on_position(invertedList_term_pointer,16UL,lastdocsize,read_Last_docIds_data,lengthOfChunksizeList,read_chunksize_data);
+                        cout<<lastdocsize<<" "<<read_Last_docIds_data<<" "<<lengthOfChunksizeList<<" "<<read_chunksize_data<<endl;
                         
-                        break;
-        }
-        total_chunk_Size += read_chunksize_data[ (2*i) ] + read_chunksize_data [ (2*i)+1 ];
+                        // BLock search based on DocID
+                        vector<unsigned long int> miniBlock_DocID;
+                        vector<unsigned long int> miniBlock_Freq;
+                        get_mini_block_DocIDs_and_Freq(invertedList_term_pointer, decompressed_docID_block[blockIndex] ,miniBlock_DocID,miniBlock_Freq,lastdocsize,read_chunksize_data,read_Last_docIds_data);
+
+                        // Condition check
+                    
+
+                        invertedList_term_pointer.close();
+                    }
+                }
             }
-    
-    }
 
-void inverted_index_DAAT()
-{
+           
 
-}
-
- int main()
- {
-    // Needed for traversal : Last DocIdSize, Last DocIDs List, ChunkSize length, ChunkSize list
-    ifstream bin_in_file("data.bin", ios::binary);
-    unsigned long int lastdocsize=0UL;
-    vector<unsigned long int> read_Last_docIds_data;
-    unsigned long int lengthOfChunksizeList=0UL;
-    vector<unsigned long int> read_chunksize_data;
-    get_inverted_index_Metadatas_based_on_position(bin_in_file,16UL,lastdocsize,read_Last_docIds_data,lengthOfChunksizeList,read_chunksize_data);
-    cout<<lastdocsize<<" "<<read_Last_docIds_data<<" "<<lengthOfChunksizeList<<" "<<read_chunksize_data<<endl;
+        }
+         }
    
-    
+            void get_mini_block_DocIDs_and_Freq( ifstream& bin_in_file, unsigned long int docID_to_be_searched, vector<unsigned long int> &miniBlock_DocID 
+                            ,vector<unsigned long int> &miniBlock_Freq,unsigned long int &lastdocsize, vector<unsigned long int> &read_chunksize_data,vector<unsigned long int> &read_Last_docIds_data)
+            {
+                // To retrieve the particular Block based on the DocID
+                // unsigned long int docID_to_be_searched;
+                unsigned long int total_chunk_Size=0UL;
+                for(unsigned long int i=0;i<lastdocsize;i++)
+                    {
+                
+                    if( docID_to_be_searched < read_Last_docIds_data[i] )
+                        {
+
+                                // Need to swift for reading
+                                bin_in_file.seekg(total_chunk_Size,ios::cur);
+
+                                // Reading that particular block and passing for uncompression (DOCIDs List and Freqs List)
+                                vector<u_int8_t> miniBlock_DocID_compressed( read_chunksize_data[2*i] );
+                                vector<u_int8_t> miniBlock_Freq_compressed( read_chunksize_data[(2*i)+1] );
+                                bin_in_file.read(reinterpret_cast<char*>( miniBlock_DocID_compressed.data()), miniBlock_DocID.size());
+                                bin_in_file.read(reinterpret_cast<char*>( miniBlock_Freq_compressed.data()), miniBlock_Freq.size());
+                                
+                                miniBlock_DocID = varbyte_decompress(miniBlock_DocID_compressed);
+                                miniBlock_Freq= varbyte_decompress(miniBlock_Freq_compressed);
+
+                                break;
+                }
+                total_chunk_Size += read_chunksize_data[ (2*i) ] + read_chunksize_data [ (2*i)+1 ];
+                    }
+                
+            
+            }
 
 
-    //Closing any instance
-    bin_in_file.close();
+        int main()
+        {
+            // Needed for traversal : Last DocIdSize, Last DocIDs List, ChunkSize length, ChunkSize list
 
-    return 0;
- }
+
+            ifstream bin_in_file("data.bin", ios::binary);
+            unsigned long int lastdocsize=0UL;
+            vector<unsigned long int> read_Last_docIds_data;
+            unsigned long int lengthOfChunksizeList=0UL;
+            vector<unsigned long int> read_chunksize_data;
+            get_inverted_index_Metadatas_based_on_position(bin_in_file,16UL,lastdocsize,read_Last_docIds_data,lengthOfChunksizeList,read_chunksize_data);
+            cout<<lastdocsize<<" "<<read_Last_docIds_data<<" "<<lengthOfChunksizeList<<" "<<read_chunksize_data<<endl;
+            
+            // Decompress block by block for the the entire term or based on the condition
+            vector<u_int8_t> entireBlock_DocID,entireBlock_Freq;
+
+            
+
+
+            
+
+
+            //Closing any instance
+            bin_in_file.close();
+
+            return 0;
+        }
